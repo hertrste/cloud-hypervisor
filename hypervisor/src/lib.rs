165e82d79b4b164231b24dbd907ddd87b9def37c
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2025-10-07 07:25:20 +0200

    hypervisor: Derive Eq for HypervisorType and CpuVendor
    Signed-Off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- hypervisor/src/lib.rs
+++ hypervisor/src/lib.rs
@@ -69,7 +69,7 @@ pub use vm::{
 
 pub use crate::hypervisor::{Hypervisor, HypervisorError};
 
-#[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
 pub enum HypervisorType {
     #[cfg(feature = "kvm")]
     Kvm,
